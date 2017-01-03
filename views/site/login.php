30c4ee37d34535b845c1177e01f971742d58ce04
HWLin <hwlin1414@gmail.com>
2017-01-03 15:33:04 +0800

    modify login page layout

--- views/site/login.php
+++ views/site/login.php
@@ -7,13 +7,11 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = '登入';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
+    <h3><?= Html::encode($this->title) ?></h3>
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
@@ -34,7 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('登入', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
 
