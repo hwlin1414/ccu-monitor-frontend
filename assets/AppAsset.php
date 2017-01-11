3aa50d13ff8f40827d7640362e6c3cd6509f828d
HWLin <hwlin1414@gmail.com>
2017-01-11 08:43:56 +0800

    update composer

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,7 +21,6 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
-        'js/materialize.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
