733e4dd4df7ebdaa19a9be13c444c40e9333b1e8
HWLin <hwlin1414@gmail.com>
2017-01-11 14:14:37 +0800

    update init data

--- migrations/m161228_022556_init_data.php
+++ migrations/m161228_022556_init_data.php
@@ -6,6 +6,22 @@ class m161228_022556_init_data extends Migration
 {
     public function up()
     {
+        $this->insert('GlobalConfig', [
+            'key' => 'defaultGroupId',
+            'value' => '1',
+        ]);
+        $this->insert('GlobalConfig', [
+            'key' => 'mailVerifycation',
+            'value' => '1',
+        ]);
+        $this->insert('GlobalConfig', [
+            'key' => 'registEnable',
+            'value' => '1',
+        ]);
+        $this->insert('GlobalConfig', [
+            'key' => 'userVerifycation',
+            'value' => '1',
+        ]);
         $this->insert('Groups', [
             'name' => '管理員',
         ]);
@@ -21,15 +37,27 @@ class m161228_022556_init_data extends Migration
             'verified' => true,
             'registedip' => '127.0.0.1',
         ]);
-        
+        $this->insert('Logs', [
+            'ip' => '0.0.0.0',
+            'level' => 'info',
+            'user_id' => null,
+            'action' => 'app',
+            'description' => '系統初始化',
+        ]);
     }
 
     public function down()
     {
         $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
+        $this->truncateTable('Services');
+        $this->truncateTable('HostContact');
+        $this->truncateTable('Hosts');
+        $this->truncateTable('Contacts');
+        $this->truncateTable('Logs');
         $this->truncateTable('Users');
         $this->truncateTable('GroupPerms');
         $this->truncateTable('Groups');
+        $this->truncateTable('GlobalConfig');
         $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
     }
 
